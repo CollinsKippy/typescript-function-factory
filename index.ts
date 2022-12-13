eb9e63ba274d27a66c5e9c45a5b784ee83a3181b
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2022-12-13 19:37:12 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -4,17 +4,13 @@ import './style.css';
 var myArray = [1, 2, 3, 4, 5];
 
 /**
- * The processNumber function returns a function that will be user-defined.
- * Its signature/parameter is a user-defined function that takes a number and returns a number
- * A function factory.
+ * The processNumber function returns a function.
+ * Its signature/parameter is a user-defined function that process a number and returns a number.
  */
 function processNumber(fn: (n: number) => number): (num: number) => number {
   return (someNumber) => fn(someNumber);
 }
 
-/**
- * The argument of the processNumber is an arrow function expression.
- */
 const myProcessor1 = processNumber((x) => x ** 2);
 const myProcessor2 = processNumber((x) => x + 10);
 const result = myProcessor1(2);
@@ -25,7 +21,7 @@ console.log({ result2 });
 
 /**
  * This function returns a function that processes arrays in manner a that the user defines.
- * The user-defined function must return another array.
+ * The user-defined function takes a number array and must return a number array.
  */
 function processNumberArray(
   fn: (n: number[]) => number[]
